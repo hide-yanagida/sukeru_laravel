e3349426d55d97634b75f56729c200bda27c3449
柳田英彦 <hide@satouhidehiko-no-MacBook-Air.local>
2018-10-07 21:17:15 +0900

    mockupの移植完了

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,52 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-
+    <meta charset="utf-8">
+    <title>SUKERU</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Loading Flat UI Pro -->
+    <link href="css/app.css" rel="stylesheet">
+    <link href="css/flat-ui.css" rel="stylesheet">
+    <link href="css/common.css" rel="stylesheet">
   </head>
   <body>
 
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <nav class="navbar navbar-inverse navbar-embossed navbar-expand-lg" role="navigation">
+            <a class="navbar-brand" href="index.html">SUKERU</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-01"></button>
+            <div class="collapse navbar-collapse" id="navbar-collapse-01">
+              <ul class="nav navbar-nav mr-auto">
+              </ul>
+              <ul class="nav navbar-nav">
+                <li><a href="request.html">
+                  <button class="btn btn-social-facebook">
+                    <span class="fui-facebook"></span> facebookで登録
+                  </button></a>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </nav><!-- /navbar -->
+        </div>
+      </div>
+
 	  @yield('content')
 
+    </div>
+    <footer class="py-5">
+      <div class="container">
+        <a href="https://twitter.com/shiwataro1?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @shiwataro1</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <p class="m-0 text-center">Copyright &copy; Hanamaki Hackathon 2018</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap 4 requires Popper.js -->
+    <script src="js/app.js"></script>
+    <script src="https://unpkg.com/popper.js@1.14.1/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="js/flat-ui.js"></script>
+    <script src="js/application.js"></script>
   </body>
 </html>
--- resources/views/pages/top.blade.php
+++ resources/views/pages/top.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.default')
 @section('content')
 
-<h1>top</h1>
+<div class="row">
+  <div class="demo-type-example col">
+    <img src="images/sukeru_top2.png" alt="example-image" class="img-rounded img-fluid">
+    <p class="img-comment"><strong>SUKERUは、"お手伝いが必要な人" と "お手伝いをしたい人"をつなぐサービスです。</strong></p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="d-flex justify-content-center sub-description col">
+    <h3>※ここに使い方を説明する絵を入れる。</h3>
+  </div>
+</div>
 
 @endsection
