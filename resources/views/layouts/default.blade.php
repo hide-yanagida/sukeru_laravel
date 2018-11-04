3805660afdd0c928627e5db2b9bc19854d0abfdd
柳田英彦 <hide@satouhidehiko-no-MacBook-Air.local>
2018-11-05 01:30:31 +0900

    shareボタンとfeedbackボタン作成

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -62,8 +62,12 @@
     </div>
     <footer class="py-5">
       <div class="container">
-
-        <a href="https://twitter.com/shiwataro1?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @shiwataro1</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <div class="row">
+          <a href="https://twitter.com/share?url=https://sukeru.help&via=shiwataro1&related=shiwataro1&hashtags=sukeru&text=SUKERU〜お手伝いをなめらかに〜" rel="nofollow" target="_blank" class="btn btn-info mb-sm-5" onclick="window.open(this.href, 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;"><span class="fui-twitter col-xs-2"></span> SUKERUをシェア</a>
+        </div>
+        <div class="row">
+          <a href="https://twitter.com/shiwataro1" target="_blank" class="btn btn-info mb-sm-5"><span class="fui-twitter col-xs-2"></span> 開発者にフィードバック</a>
+        </div>
         <p class="m-0 text-center">Copyright &copy; Hanamaki Hackathon 2018</p>
       </div>
     </footer>
